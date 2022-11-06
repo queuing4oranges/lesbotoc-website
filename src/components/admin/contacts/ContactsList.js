1be2d2630362ccb365e4d1b0a4347e7566504a46
queuing4oranges <katjazenker@gmail.com>
2022-11-06 13:50:42 +0100

    toggle forms

--- src/components/admin/contacts/ContactsList.js
+++ src/components/admin/contacts/ContactsList.js
@@ -18,7 +18,7 @@ export default function ContactsList() {
 
   useEffect(() => {
     getContacts();
-  }, [])  
+  }, [contactsLoaded])  
  
   function getContacts() {
     axios.get('https://api.itisgoodtohave.me/contacts/read.php')
@@ -36,10 +36,8 @@ export default function ContactsList() {
       alert("Do you want to delete that contact?");  //needs a cancel opt!
       getContacts();
       setContactsLoaded(false);
-
     })
   }
-
 
   function toggleAddField () {
     if (addField === true) {
@@ -48,8 +46,11 @@ export default function ContactsList() {
       } else {
       setAddField(true);
       setButtonText("Cancel")
-      }
-    }
+      emptyInputs();
+      }   
+      getContacts();   
+      setContactsLoaded(false)
+  }
 
   function toggleEditField () {
     if (editField === true) {
@@ -59,12 +60,12 @@ export default function ContactsList() {
     }
   }
 
-      function emptyInputs () {
+    function emptyInputs () {
     let elements = document.querySelectorAll(".input-item")
     elements.forEach((element) =>{
          element.value = "";
     })
-    }
+  }
 
   return (
     <Fragment>
@@ -82,9 +83,7 @@ export default function ContactsList() {
     
       <div className="edit-cont">
         <div className={editField ? "show" : "hide"}><EditContact  />
-
         </div>
-
       </div>
 
     </div>
@@ -116,8 +115,11 @@ export default function ContactsList() {
               <td className="td td-newsletter">{(contact.newsletter === 0) ? "no" : "yes"}</td>
               <td className="td td-age">{contact.age}</td>
               <td className="td td-crud">
-                <Link to={`${contact.id}/edit`}><button className="btn btn-info">Edit</button></Link>
-                <button className="btn btn-danger" id={contact.id} onClick={() =>deleteContact(contact.id)}>Delete</button>
+                <Link to={`${contact.id}/edit`}><button className="btn btn-info btn-sm">Edit</button></Link>
+                <button className="btn btn-danger btn-sm" id={contact.id} onClick={() =>deleteContact(contact.id)}><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
+  <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+  <path fillRule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+</svg></button>
               </td>
               </tr>
               ))}
