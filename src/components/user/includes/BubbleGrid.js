0067095af20c29eeafe89f2cee109e567b925155
queuing4oranges <katjazenker@gmail.com>
2023-01-17 22:20:19 +0100

    fixed bubblegrid

--- src/components/user/includes/BubbleGrid.js
+++ src/components/user/includes/BubbleGrid.js
@@ -1,31 +1,39 @@
-import React from 'react';
+import React from "react";
+import { useState, useEffect } from "react";
 
 export default function BubbleGrid(props) {
+  const [bubbles, setBubbles] = useState([]);
 
-  
+  useEffect(() => {
+    countBubbles();
+  }, []);
+
+  //looping 1-21 (amount bubbles)
+  const countBubbles = () => {
+    const bubbleArr = [];
+    for (let i = 1; i < 22; i++) {
+      bubbleArr.push(i);
+    }
+    console.log(bubbleArr);
+    //setting bubbles to number array 1-21
+    setBubbles(bubbleArr);
+  };
+
+  console.log(bubbles);
+
+  //make container for each bubble by mapping through bubbles
   return (
     <div className="bubble-grid">
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-          <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-  </div>
-  )
+      {bubbles &&
+        bubbles.map((key) => (
+          <div
+            key={key}
+            className="bubble"
+            style={{ backgroundColor: props.color }}
+          >
+            {props.children}
+          </div>
+        ))}
+    </div>
+  );
 }
--- src/components/user/includes/BubbleGrid6x7.js
+++ src/components/user/includes/BubbleGrid6x7.js
@@ -1,29 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
-export default function BubbleGrid6x7() {
+export default function BubbleGrid6x7(props) {
+  const [bubbles, setBubbles] = useState([]);
+
+  useEffect(() => {
+    countBubbles();
+  }, []);
+
+  const countBubbles = () => {
+    const bubbleArray = [];
+    for (let i = 1; i < 43; i++) {
+      bubbleArray.push(i);
+    }
+    console.log(bubbleArray);
+    setBubbles(bubbleArray);
+  };
+
   return (
     <div className="bubble-grid6x7">
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
-        <div className="bubble" style={{backgroundColor: props.color}}>{props.children}</div>
+      {bubbles &&
+        bubbles.map((key) => (
+          <div
+            key={key}
+            className="bubble6x7"
+            style={{ backgroundColor: props.color }}
+          >
+            {props.children}
+          </div>
+        ))}
     </div>
-  )
+  );
 }
