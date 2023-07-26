32d797c4021d2df20050690bb9cedf7505b88655
queuing4oranges <katjazenker@gmail.com>
2023-07-26 23:30:08 +0200

    responsive home component

--- src/components/user/home/Home.js
+++ src/components/user/home/Home.js
@@ -1,5 +1,5 @@
 import { HomeGradient } from "./HomeGradient";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 //logo
@@ -14,6 +14,16 @@ export default function Home() {
   const navigate = useNavigate();
   const [title, setTitle] = useState("");
   const [fontColor, setFontColor] = useState("#003243");
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  const eventImages = links.map((link) => link.background);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % eventImages.length);
+    }, 2500);
+    return () => clearInterval(interval);
+  }, [eventImages]);
 
   const handleMouseEnter = (newTitle) => {
     setTitle(newTitle);
@@ -65,6 +75,21 @@ export default function Home() {
             ></div>
           ))}
         </div>{" "}
+        {/* the mobile-home-cont will be only visible on mobiles and should have changing images */}
+        <div className="mobile-home-cont">
+          {eventImages.map((img, index) => (
+            <img
+              className="event-images"
+              key={index}
+              src={img}
+              alt={index}
+              style={{ display: index === currentIndex ? "block" : "none" }}
+            />
+          ))}
+          <div className="enter-btn" onClick={() => goToCalendar()}>
+            <RightArrow height={50} width={50} />
+          </div>
+        </div>
       </div>
       <div className="dynamic-title-cont">
         <p
