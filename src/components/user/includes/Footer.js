6f349504573ccf9cef9a026515225c062bc19c0c
queuing4oranges <katjazenker@gmail.com>
2023-12-14 22:41:22 +0100

    Refactor Footer and scss to reactstrap

--- src/components/user/includes/Footer.js
+++ src/components/user/includes/Footer.js
@@ -1,76 +1,59 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { Container, Col, Row } from "reactstrap";
 
 export default function Footer() {
-  //getting updated year for footer
-  const currentYear = new Date();
+//getting updated year for footer
+const currentYear = new Date();
 
-  return (
-    <div className="user-footer-container">
-      <div className="user-footer-cont">
-        <div className="user-footer-copyright">
-          <div className="copyright-lesbotoc">
-            <p>&copy; {currentYear.getFullYear()} &nbsp; Lesbotoč </p>
-          </div>
-          <div className="copyright-katja">
-            <p>made by</p>
-            <a
-              href="https://www.itisgoodtohave.me"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Portfolio Katja Zenker"
-            >
-              &nbsp;Katja Zenker
-            </a>
-          </div>
-        </div>
-
-        {/* <div className="user-footer-newsletter">
-          <p className="footer-privacy">Privacy</p>
-        </div> */}
-
-        <div className="user-footer-admin">
-          <div className="user-footer-social">
-            <a
-              href="https://www.facebook.com/seznamsenatoci"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Facebook"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                fill="currentColor"
-                className="bi bi-facebook user-navbar-fb"
-                viewBox="0 0 16 16"
-              >
-                <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z" />
-              </svg>
-            </a>
-            <a
-              href="https://www.instagram.com/lesbotoc/"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Instagram"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="32"
-                height="32"
-                fill="currentColor"
-                className="bi bi-instagram user-navbar-insta"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 0C5.829 0 5.556.01 4.703.048 3.85.088 3.269.222 2.76.42a3.917 3.917 0 0 0-1.417.923A3.927 3.927 0 0 0 .42 2.76C.222 3.268.087 3.85.048 4.7.01 5.555 0 5.827 0 8.001c0 2.172.01 2.444.048 3.297.04.852.174 1.433.372 1.942.205.526.478.972.923 1.417.444.445.89.719 1.416.923.51.198 1.09.333 1.942.372C5.555 15.99 5.827 16 8 16s2.444-.01 3.298-.048c.851-.04 1.434-.174 1.943-.372a3.916 3.916 0 0 0 1.416-.923c.445-.445.718-.891.923-1.417.197-.509.332-1.09.372-1.942C15.99 10.445 16 10.173 16 8s-.01-2.445-.048-3.299c-.04-.851-.175-1.433-.372-1.941a3.926 3.926 0 0 0-.923-1.417A3.911 3.911 0 0 0 13.24.42c-.51-.198-1.092-.333-1.943-.372C10.443.01 10.172 0 7.998 0h.003zm-.717 1.442h.718c2.136 0 2.389.007 3.232.046.78.035 1.204.166 1.486.275.373.145.64.319.92.599.28.28.453.546.598.92.11.281.24.705.275 1.485.039.843.047 1.096.047 3.231s-.008 2.389-.047 3.232c-.035.78-.166 1.203-.275 1.485a2.47 2.47 0 0 1-.599.919c-.28.28-.546.453-.92.598-.28.11-.704.24-1.485.276-.843.038-1.096.047-3.232.047s-2.39-.009-3.233-.047c-.78-.036-1.203-.166-1.485-.276a2.478 2.478 0 0 1-.92-.598 2.48 2.48 0 0 1-.6-.92c-.109-.281-.24-.705-.275-1.485-.038-.843-.046-1.096-.046-3.233 0-2.136.008-2.388.046-3.231.036-.78.166-1.204.276-1.486.145-.373.319-.64.599-.92.28-.28.546-.453.92-.598.282-.11.705-.24 1.485-.276.738-.034 1.024-.044 2.515-.045v.002zm4.988 1.328a.96.96 0 1 0 0 1.92.96.96 0 0 0 0-1.92zm-4.27 1.122a4.109 4.109 0 1 0 0 8.217 4.109 4.109 0 0 0 0-8.217zm0 1.441a2.667 2.667 0 1 1 0 5.334 2.667 2.667 0 0 1 0-5.334z" />
-              </svg>
-            </a>
-          </div>
-          {/* <NavLink to={"/admin"}> */}
-          <button className="btn admin-panel-button">AdminPanel</button>
-          {/* </NavLink> */}
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<Container fluid className="footer-wrapper h-100">
+			<Row>
+				<Col md="8">					
+					<div className="d-flex flex-column justify-content-center align-items-start copyright-katja">
+						<p className="pt-3 mb-0">&copy; {currentYear.getFullYear()}&nbsp; Lesbotoč </p>
+						<p>made by 						
+							<a
+								href="https://www.queuing4oranges.com/"
+								target="_blank"
+								rel="noreferrer"
+								aria-label="Portfolio Katja Zenker"
+								className="text-danger"
+							>
+							&nbsp;Katja Zenker
+							</a>   
+						</p>
+					</div>
+				</Col>
+				
+				<Col md="2" className="d-flex align-items-center justify-content-center">
+					<div>
+						<a
+							href="https://www.facebook.com/seznamsenatoci"
+							target="_blank"
+							rel="noreferrer"
+							aria-label="Facebook"
+							className="mr-2"
+						>
+							<i className="bi bi-facebook" style={{fontSize: "2rem", color: "#003243"}}></i>
+						</a>
+						
+						<a
+							href="https://www.instagram.com/lesbotoc/"
+							target="_blank"
+							rel="noreferrer"
+							aria-label="Instagram"
+							className="ml-2"
+						>
+							<i class="bi bi-instagram" style={{fontSize: "2rem", color: "#003243"}}></i>
+						</a>
+					</div>
+				</Col>
+				
+				<Col md="2" className="d-flex align-items-center justify-content-center">
+					{/*TODO <NavLink to={"/admin"}></NavLink> */}
+					<button className="btn admin-panel-button">AdminPanel</button>
+				</Col>
+			</Row>
+		</Container>
+	);
 }
