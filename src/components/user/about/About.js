c7ec9eeb4952e9d975cc782a69a8d068f54c17c8
queuing4oranges <katjazenker@gmail.com>
2024-01-09 23:12:07 +0100

    Refactor about screen, add member, clean up scss

--- src/components/user/about/About.js
+++ src/components/user/about/About.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import Footer from "../includes/Footer";
 import Navbar from "../includes/Navbar";
-import team from "../../../assets/about_images/team2.png";
+import team from "../../../assets/about_images/team2.avif";
 import { members } from "./aboutData";
 import { Spinner } from "../includes/Spinner";
 
@@ -15,51 +15,47 @@ export default function About() {
 		}, 500);
 	}, []);
 
+	if (loading) {
+		return <Spinner/>
+	}
+
 	return (
-		<>
-			{loading ? (
-				<Spinner />
-			) : (
-				<div className="user-container">
-					<Navbar />
-					<div className="user-about-cont">
-						<h2 className="user-title">To jsme my</h2>
-					<img
-						src={team}
-						alt="3 women from lesbotoč"
-						className="user-about-img"
-						loading="eager"
-					/>
-				</div>
+		<div className="screen-wrapper">
+			<Navbar />
 
-				<div className="user-about-intro">
-					<p className="user-paragraph user-about-paragraph">
-						Jsme tři obyčejné ženské. Jedna z Prahy, druhá z Moravy a třetí od
-						Plzně. Sešly jsme se z různých koutů ČR, abychom uspořádaly tu
-						pravou seznamovací akci. Zjistily jsme totiž po letech úporného
-						hledání, že lesby nemají pořádně kam jít a kde se najít. Chceme
-						vytvářet prostor pro seznamovací akce pravidelně každý měsíc.
-						Nenásilnou formou tak chceme docílit, abychom se všechny poznaly a
-						nezbylo nám jediného opomíjeného živáčka v koutě.
-					</p>
-				</div>
+			<h2 className="screen-title">To jsme my</h2>
 
-				<div className="user-singles-container">
-					<div className="user-singles">
-						{members.map((member) => (
-							<div key={member.id} className="user-about">
-							<img src={member.img} loading="lazy" alt={member.name} />
-							<p className="member-name">{member.name}</p>
-							<p className="user-paragraph">{member.intro}</p>
-							</div>
-						))}
+			<img
+				src={team}
+				alt="3 women from lesbotoč"
+				className="w-100"
+				loading="eager"
+			/>
+
+			<div>
+				<p className="mt-4 mb-4 pb-5 mx-auto about-paragraph text-start">
+					Jsme tři obyčejné ženské. Jedna z Prahy, druhá z Moravy a třetí od
+					Plzně. Sešly jsme se z různých koutů ČR, abychom uspořádaly tu
+					pravou seznamovací akci. Zjistily jsme totiž po letech úporného
+					hledání, že lesby nemají pořádně kam jít a kde se najít. Chceme
+					vytvářet prostor pro seznamovací akce pravidelně každý měsíc.
+					Nenásilnou formou tak chceme docílit, abychom se všechny poznaly a
+					nezbylo nám jediného opomíjeného živáčka v koutě.
+				</p>
+			</div>
+
+			<div className="user-singles w-100 d-flex flex-wrap justify-content-center">
+				{members.map((member) => (
+					<div key={member.id} className="user-about d-flex flex-column align-items-center p-3">
+					<img src={member.img} loading="lazy" alt={member.name} />
+					<p className="member-name mt-3">{member.name}</p>
+					<p className="user-paragraph text-start">{member.intro}</p>
 					</div>
-				</div>
-				
+				))}
+			</div>
+
 			<Footer />
 			
-			</div>
-			)}
-		</>
+		</div>
 	);
 }
